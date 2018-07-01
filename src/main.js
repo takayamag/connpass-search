0b53e2b5daaac6024b1a979f6e5c5c9efa09dfb6
takayamag <tak.yama.dev@gmail.com>
2018-07-01 20:53:16 +0900

    コメントを追加

--- src/main.js
+++ src/main.js
@@ -5,6 +5,9 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
+
+// Element のライブラリを使用する
+// http://element.eleme.io/#/en-US
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import ElementUIJaLocale from 'element-ui/lib/locale/lang/ja'
